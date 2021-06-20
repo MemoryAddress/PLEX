6ad29e15087630a81c3a8fefd91c58654250fab1
MemoryAddress <86206652+MemoryAddress@users.noreply.github.com>
2021-06-20 16:57:41 -0400

    Update PlexCore.lua

--- PlexCore.lua
+++ PlexCore.lua
@@ -1186,8 +1186,10 @@ AIMBOT.TargetPart = nil
 function AIMBOT.LockOn(PART)
 	if (GLOBAL.IsAlive()) then
 		if (isrbxactive()) then
-			mousemoverel((workspace.CurrentCamera:WorldToScreenPoint(PART.CFrame.p).X - workspace.CurrentCamera:WorldToScreenPoint(game:GetService("Players").LocalPlayer:GetMouse().Hit.p).X), (workspace.CurrentCamera:WorldToScreenPoint(PART.CFrame.p).Y - workspace.CurrentCamera:WorldToScreenPoint(game:GetService("Players").LocalPlayer:GetMouse().Hit.p).Y))
-			game:GetService("RunService").RenderStepped:Wait()
+			for i=1, 4 do
+				mousemoverel((workspace.CurrentCamera:WorldToScreenPoint(PART.CFrame.p).X - workspace.CurrentCamera:WorldToScreenPoint(game:GetService("Players").LocalPlayer:GetMouse().Hit.p).X)/4, (workspace.CurrentCamera:WorldToScreenPoint(PART.CFrame.p).Y - workspace.CurrentCamera:WorldToScreenPoint(game:GetService("Players").LocalPlayer:GetMouse().Hit.p).Y)/4)
+				game:GetService("RunService").RenderStepped:Wait()
+			end
 		end
 	end
 	--workspace.CurrentCamera.CFrame = CFrame.new(workspace.CurrentCamera.CFrame.p, PART.CFrame.p) old aimbot
